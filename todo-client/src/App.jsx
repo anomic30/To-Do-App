16cb330bdd74639a40eeee26990cfbad2b2e7c6a
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2022-07-15 20:33:35 +0530

    added tile component + more styling

--- todo-client/src/App.jsx
+++ todo-client/src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
+import Tile from './components/tiles/Tile';
 import './App.css'
 
 const App = () => {
+  const [list, setList] = useState(Array(9).fill(0));
   return (
     <div className="App">
       <header>
-        <h1>To-Do List</h1>
+        <p>To-Do List</p>
       </header>
 
       <section className='add-task-con'> 
         <p>Add a new task in the list</p>
         <div className="inp-con">
           <input type="text" placeholder="Enter the task here" />
-          <button>Submit</button>
+          <div className="button">Submit</div>
+        </div>
+      </section>
+
+      <section className='todos-con'>
+        <div className="action-bar">
+          <p>Added task in to to-do list</p>
+          <p>Filter: Completed</p>
+        </div>
+        <div className="todos-list">
+          {list.map((val, idx) => {
+            return <div className='tile-wrapper'>
+              <p className='num'>{idx+1}.</p>
+              <Tile key={idx}/>
+            </div>
+          })}
         </div>
       </section>
     </div>
--- todo-client/src/components/tiles/Tile.jsx
+++ todo-client/src/components/tiles/Tile.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Tile.css'
+import checkmark from '../../assets/checkmark.svg'
 
-const Tile = () => {
+const Tile = ({todo}) => {
   return (
-    <div>Tile</div>
+    <div className='tile-con completed'>
+      <img src={checkmark} alt="" className='checkmark'/>
+      <div className='tile-header'>
+        <p>Task 1</p>
+      </div>
+
+      <div className='tile-actions'>
+        <div className="button remove">Mark as completed</div>
+        <p>Delete</p>
+      </div>
+
+    </div>
   )
 }
 
